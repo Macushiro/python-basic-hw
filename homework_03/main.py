aff0b3289f63cd2d680f6aeae5c0553d17255b3a
Macushiro <tolmachevkonstantin@gmail.com>
2022-11-13 20:31:47 +0300

    Added new route - /get_current_dts, and service starting description.

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,6 +1,7 @@
 """
     Description: Main module for API.
-    To run this service, just execute this command in terminal:  uvicorn main:app
+    To run this service locally, just execute this command in terminal:  uvicorn main:app
+    To run this service in docker, just execute this command in terminal:  docker run -it -p 8000:8000 app
 """
 from fastapi import APIRouter, FastAPI
 
--- homework_03/views/base_route.py
+++ homework_03/views/base_route.py
@@ -4,6 +4,7 @@
 from fastapi import APIRouter
 from starlette import status
 from starlette.responses import JSONResponse
+from datetime import datetime
 
 
 router = APIRouter(
@@ -16,6 +17,17 @@ def main_response() -> JSONResponse:
     return JSONResponse(
         {
             "message": "pong",
+        },
+        status_code=status.HTTP_200_OK
+    )
+
+@router.get('/get_current_dts')
+def get_current_time() -> JSONResponse:
+    now = datetime.now()
+    return JSONResponse(
+        {
+            "Current date is": now.strftime('%Y-%m-%d'),
+            "Current time is": now.strftime('%H:%M:%S'),
         },
         status_code=status.HTTP_200_OK
     )
