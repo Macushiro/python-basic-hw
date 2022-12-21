1bb0fc5224947e994de183cd7cfaf5dd5fc3acc4
Macushiro <tolmachevkonstantin@gmail.com>
2022-12-21 22:47:29 +0300

    Added first functions to use aiohttp.

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,36 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+import asyncio
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+import aiohttp
+
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+URLS = [USERS_DATA_URL, POSTS_DATA_URL]     # for next step for near OOP style
+
+
+async def fetch_json(url):
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        data = await response.json()
+        print(data)
+        return data
+
+
+async def main():
+    # tasks = set()
+    tasks = {asyncio.create_task(fetch_json(el)) for el in URLS}
+    # tasks.add(asyncio.create_task(fetch_json(urls[0])))
+    # tasks.add(asyncio.create_task(fetch_json(POSTS_DATA_URL)))
+    # for el in urls:
+    #     tasks.add(asyncio.create_task(fetch_json(el)))
+    await asyncio.wait(tasks)
+
+
+if __name__ == '__main__':
+    # asyncio.run(fetch_json(USERS_DATA_URL))
+    # asyncio.run(fetch_json(POSTS_DATA_URL))
+    asyncio.run(main())
