db92e4aa737d74410ff39807ac6931fe1965931e
Macushiro <tolmachevkonstantin@gmail.com>
2022-12-21 22:51:41 +0300

    Deleted draft code in functions.

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -16,21 +16,13 @@ async def fetch_json(url):
     async with aiohttp.ClientSession() as session:
         response = await session.get(url)
         data = await response.json()
-        print(data)
         return data
 
 
 async def main():
-    # tasks = set()
     tasks = {asyncio.create_task(fetch_json(el)) for el in URLS}
-    # tasks.add(asyncio.create_task(fetch_json(urls[0])))
-    # tasks.add(asyncio.create_task(fetch_json(POSTS_DATA_URL)))
-    # for el in urls:
-    #     tasks.add(asyncio.create_task(fetch_json(el)))
     await asyncio.wait(tasks)
 
 
 if __name__ == '__main__':
-    # asyncio.run(fetch_json(USERS_DATA_URL))
-    # asyncio.run(fetch_json(POSTS_DATA_URL))
     asyncio.run(main())
