12839429d11f661b54e9b735d8bc07c46c8f7b0e
Macushiro <tolmachevkonstantin@gmail.com>
2023-01-19 22:59:01 +0300

    Added migrations and additional dependencies (homework_06).

--- homework_06/app/app.py
+++ homework_06/app/app.py
@@ -5,6 +5,7 @@
 from os import getenv
 
 from flask import Flask, render_template
+from flask_migrate import Migrate
 
 from views.users import users_app
 from views.bills import bills_app
@@ -21,12 +22,7 @@ app.config.from_object(f"config.{CONFIG_OBJECT}")
 
 
 db.init_app(app)
-
-@app.cli.command("db-create-all")
-def db_create_all():
-    db.drop_all()
-    db.create_all()
-
+migrate = Migrate(app, db)
 
 @app.get("/", endpoint="index_page")
 def get_root():
